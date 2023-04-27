733550e3f4a2f5d3d73972c9e6d979509e302ce1
Thanesh Pannirselvam <thanesh@Thaneshs-MacBook-Pro.local>
2023-04-27 20:41:00 +1000

    Adds HTML to display background image

--- meet_me_halfway/settings.py
+++ meet_me_halfway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+APP_STATIC_DIR = os.path.join(BASE_DIR, 'app', 'templates', 'static')
+STATICFILES_DIRS = [APP_STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
